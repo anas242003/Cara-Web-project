58438209d0d4510663ba54a967982bb188d009a6
mohamedfahd12345 <momoelofy@gmail.com>
2024-05-03 22:43:18 +0300

    Added: Product CRUD - with authorization admin role vaildation

---
+++ routes/product.routes.js
@@ -0,0 +1,150 @@
+const router = require("express").Router();
+const Product = require('../models/Product.model');
+const mongoose = require("mongoose");
+const { authMiddleware } = require('../middleware/jwt.middleware');
+const { isAdmin, isCustomer } = require('../middleware/guard.middleware.js');
+
+
+// Get all  products  in database
+router.get('/products', async (req, res, next) => {
+
+    try {
+        console.log("hereeeeeeeee 111 ");
+        const products = await Product.find();
+
+        res.json(products);
+
+    } catch (error) {
+        console.log(err);
+        res.status(500).json({ message: "Failed to get all products." });
+    }
+})
+
+
+// Get product by product id 
+router.get('/products/:productId', async (req, res, next) => {
+    console.log("hereeeeeeeee 222 ");
+    const { productId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+        res.status(400).json({ message: "product id is not valid" });
+        return;
+    }
+
+    try {
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return res.status(404).json({ message: `Product with Product ID - ${productId} is not found.` });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to get the product" });
+    }
+
+})
+
+
+// Get products with paging 
+router.get('/productspage', async (req, res, next) => {
+
+    try {
+        const page = parseInt(req.query._page) || 1;
+        const pageSize = 6;
+
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+
+        const products = await Product.find().skip(startIndex).limit(pageSize);
+        const totalProductsCount = await Product.countDocuments();
+        const isLastPage = endIndex >= totalProductsCount;
+
+        res.json(products);
+
+    } catch (error) {
+        console.log(err);
+        res.status(500).json({ message: "Failed to get all products." });
+    }
+})
+//=======================================================================================
+router.post('/products', authMiddleware, isAdmin, async (req, res, next) => {
+    try {
+        console.log("hereeeeeeeee 3333333 ");
+        const { productName, price, description, image, quantity } = req.body;
+        // Validate request body
+        if (!productName || !price || !description || !quantity) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        console.log(productName);
+        console.log(price);
+        console.log(description);
+        console.log(image);
+        console.log(quantity);
+        const product = await Product.create({ productName, price, description, image, quantity });
+        res.status(201).json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to create the product" });
+    }
+});
+//=======================================================================================
+router.delete('/products/:productId', authMiddleware, isAdmin, async (req, res, next) => {
+    const { productId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+        res.status(400).json({ message: "product id is not valid" });
+        return;
+    }
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(productId);
+        if (!deletedProduct) {
+            return res.status(404).json({ message: `Product with Product ID - ${productId} is not found.` });
+        }
+        res.json({ message: `Product with ${productId} is removed successfully.` })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: `Failed to update the product with productId: ${productId}` });
+    }
+})
+//=======================================================================================
+// PUT endpoint to update an existing product
+router.put('/products/:productId', authMiddleware, isAdmin, async (req, res, next) => {
+    try {
+        const productId = req.params.productId;
+        const { productName, price, description, image, quantity } = req.body;
+        console.log(productName);
+        console.log(price);
+        console.log(description);
+        console.log(image);
+        console.log(quantity);
+        // Validate request body
+        if (!productName || !price || !description || !quantity) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+
+        // Find the existing product by ID
+        const existingProduct = await Product.findById(productId);
+
+        // If product not found, return 404 Not Found
+        if (!existingProduct) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+
+        // Update product properties
+        existingProduct.productName = productName;
+        existingProduct.price = price;
+        existingProduct.description = description;
+        existingProduct.image = image;
+        existingProduct.quantity = quantity;
+
+        // Save the updated product to the database
+        const updatedProduct = await existingProduct.save();
+
+        res.json(updatedProduct);
+    } catch (error) {
+        console.error('Error updating product:', error);
+        res.status(500).json({ error: 'Failed to update product' });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,9 +10,13 @@ dbconnect();
 
 // Middleware
 app.use(express.json());
-
 // Mount user routes
 app.use('/api/users', userRoutes);
+
+
+const productRouter = require("./routes/product.routes");
+app.use("/api", productRouter);
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
